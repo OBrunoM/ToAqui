ff74ea18259e00d9db1251adf836bd7668c95a9b
Bruno Mendonça <obrunomrodrigues@gmail.com>
2026-07-31 20:14:07 -0300

    feat: wire anonymous auth and FCM registration into app startup

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'services/auth_service.dart';
+import 'services/fcm_service.dart';
 
 import 'theme/app_theme.dart';
 import 'screens/home_screen.dart';
@@ -14,9 +18,11 @@ import 'screens/join_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final uid = await AuthService(FirebaseAuth.instance).signInAnonymously();
+  await FcmService(FirebaseFirestore.instance).registerToken(uid);
   runApp(const ProviderScope(child: ToAquiApp()));
 }
 
